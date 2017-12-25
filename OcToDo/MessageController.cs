017f7dcc15e52859a5ed7afcbb3a4a82d5806e18
Владислав Выговский <vechik.korosten@gmail.com>
2017-12-25 22:27:32 +0300

    Добавлен контроллер для обработки входящих сообщений

---
+++ OcToDo/MessageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+
+namespace OcToDo
+{
+    public static class MessageController
+    {
+        public static async void Update(object sender,MessageEventArgs eventArgs)
+        {
+            var commands = Bot.Commands;
+            Message messeage = eventArgs.Message;
+            var client = await Bot.GetTask();
+            foreach (var command in commands)
+            {
+                if (command.Contains(messeage.Text))
+                {
+                    command.Execute(messeage, client);
+                    break;
+                }
+            }
+        }
+    }
+}
