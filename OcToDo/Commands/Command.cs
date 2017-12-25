3fbe87ab0d2822649fa0e8145f23196d8f38aea4
Владислав Выговский <vechik.korosten@gmail.com>
2017-12-25 22:21:20 +0300

    Добавлен абстрактный класс Command Добавлена фактическая реализация класс HelloCommand

---
+++ OcToDo/Commands/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace OcToDo.Commands
+{
+    public abstract class Command
+    {
+        public abstract string Name { get; }
+
+        public abstract void Execute(Message message, TelegramBotClient client);
+
+        public bool Contains(string command)
+        {
+            return command.Contains(Name);
+        }
+    }
+}
---
+++ OcToDo/Commands/HelloCommand.cs
@@ -0,0 +1,21 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace OcToDo.Commands
+{
+    class HelloCommand : Command
+    {
+        public override string Name => "hello";
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+            var chatId = message.Chat.Id;
+            var messageId = message.MessageId;
+
+            await client.SendTextMessageAsync(chatId,
+                "Добро пожаловать в OcToDo, предлагаю вам авторизоваться или зарегистрироваться",
+                replyToMessageId: messageId);
+        }
+        //TODO:Добавить обработку регистрации.
+    }
+}
