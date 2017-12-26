341790b634dc5f2f3e32834cdd6a75796a8dcec3
Владислав Выговский <vechik.korosten@gmail.com>
2017-12-26 22:19:27 +0300

    Класс с функционалом отвечающим за взаимодействие с сущностью People

---
+++ OcToDo/Data/DataBase/PeopleEntity.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Linq;
+using System.Linq;
+
+namespace OcToDo.Data.DataBase
+{
+    public class PeopleEntity
+    {
+        public void Register(string Login,string Password,string Telegram_ID)
+        {
+            OcToDoDataContext db = new OcToDoDataContext();
+            db.GetTable<People>().InsertOnSubmit(new People() {Login = Login, Password = Password, Telegram_ID = Telegram_ID});
+        }
+
+        public bool Register(string Telegram_ID)
+        {
+
+            OcToDoDataContext dbContext = new OcToDoDataContext();
+
+            var username = (from un in dbContext.People
+                where un.Telegram_ID == Telegram_ID
+                select un).ToArray();
+
+            if (username[0].Telegram_ID==Telegram_ID)
+            {
+                return false;
+            }
+            if(username[0].Telegram_ID!=Telegram_ID)
+            {
+                dbContext.GetTable<People>().InsertOnSubmit(new People() { Telegram_ID = Telegram_ID });
+                dbContext.SubmitChanges();
+            }
+
+            return true;
+        }
+
+        public void Register(string FName, string MName, string LName, string Adress, string Phone, string BirthDate,
+            string Telegram_ID, string Login, string Password)
+        {
+
+        }
+    }
+}
