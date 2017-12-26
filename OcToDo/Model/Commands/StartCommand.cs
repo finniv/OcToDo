689d78651c4c1a7c5425b8f792f374320463f437
Владислав Выговский <vechik.korosten@gmail.com>
2017-12-26 22:22:31 +0300

    Переименован

---
+++ OcToDo/Model/Commands/StartCommand.cs
@@ -0,0 +1,21 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace OcToDo.Model.Commands
+{
+    class StartCommand : Command
+    {
+        public override string Name => "start";
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+            var chatId = message.Chat.Id;
+            var messageId = message.MessageId;
+
+            await client.SendTextMessageAsync(chatId,
+                "Добро пожаловать в OcToDo, предлагаю вам авторизоваться или зарегистрироваться для этого введите /register или /login",
+                replyToMessageId: messageId);
+        }
+        //TODO:Добавить обработку регистрации.
+    }
+}
